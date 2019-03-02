6c677b38cb3e6ec61d7203d9821fadc5c18c12d9
Juhani <jotain>
2019-03-02 12:31:55 +0200

    Added main, customer and training pages and table styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+import Main from './components/main/Main';
+import Customer from './components/customer/Customer';
+import Training from './components/training/Training';
+import IconLink from './IconLink';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>Lopputyö</p>
+          <p>Personal Trainer</p>
         </header>
+        <BrowserRouter>
+          <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <Link className="navbar-brand" to="/">
+                <IconLink />
+              </Link>
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/customers">Customers</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/training">Training</Link>
+                </li>
+              </ul>
+            </nav>
+            <Route exact path="/" component={Main} />
+            <Route path="/customers" component={Customer} />
+            <Route path="/training" component={Training} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
